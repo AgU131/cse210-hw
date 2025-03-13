3535a29d6e60bacf6bd77bc07ef20c9998daf3f9
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-03-13 14:52:34 -0300

    exercise1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        // Ask for the first name
+        Console.WriteLine("What is your first name? ");
         string firstName = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
+
+        // Ask for the last name
+        Console.WriteLine("What is your last name? ");
         string lastName = Console.ReadLine();
+
+        // Return the full name
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
