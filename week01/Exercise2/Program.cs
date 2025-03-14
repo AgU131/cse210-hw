176ef0618f04e09ffe7496c57224cf0c60121044
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-03-14 13:28:42 -0300

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int percent = int.Parse(gradePercent);
+
+        // Calculate grade in a letter
+        char letter = 'F';
+        if (percent >= 90)
+        {
+            letter = 'A';
+        }
+        else if (percent >= 80)
+        {
+            letter = 'B';
+        }
+        else if (percent >= 70)
+        {
+            letter = 'C';
+        }
+        else if (percent >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"You got an {letter}.");
+
+        // Last motivational message acordign to the grade
+        if (percent >= 70)
+        {
+            Console.WriteLine("Great job! You passed the class! Keep up the good work!");
+        }
+        else
+        {
+            Console.WriteLine("You didin't pass the class. Don't give up! You need to study more!");
+        }
     }
 }
