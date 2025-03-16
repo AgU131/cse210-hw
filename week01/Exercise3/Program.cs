1a180cd06487c0e0c607d286bc8adb37467d70dd
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-03-15 23:11:34 -0300

    ex 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // string response = "yes";
+        // while (response == "yes")
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine();
+        // }
+
+        // Asking for the magic number
+        // Console.Write("what is the magic number? ");
+        // string magical = Console.ReadLine();
+        // int magicNumber = int.Parse(magical);
+        // Console.WriteLine($"magic number: {magicNumber} ");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine($"Random number: {magicNumber}");
+
+        int guess = 0;
+        string answer = "";
+
+        int attempt = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess? ");
+            answer = Console.ReadLine();
+            guess = int.Parse(answer);
+            attempt++;
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Higher!");
+            }
+        }
+        Console.Write($"It took you {attempt} attempts to guess the magic number!");
+        
     }
 }
